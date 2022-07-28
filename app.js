0452eb79163b4da3627bc3ffc819fe9c252a83ad
alpo6259 <alpo6259@gmail.com>
2022-07-28 15:01:41 +0300

    add sample

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const { errorHandlers } = require('./middleware');
+const router = require('./router');
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', router);
 
-// app.use()
+app.use(errorHandlers.errorHandler);
 
 module.exports = app;
